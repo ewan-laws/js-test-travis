9bc23e9f725a05613663809678284e21706c7874
Matthew Ewan-Laws <matt.ewanlaws@gmail.com>
2020-07-13 14:30:34 +0100

    fixed travis, completed tests

--- test/clone-object.js
+++ test/clone-object.js
@@ -1,7 +1,33 @@
+
+const cloneShallow = obj => Object.assign({}, obj);
+
+function cloneDeep(val) {
+  const isArray = Array.isArray(val);
+  const isObject = typeof val === "object" && !isArray;
+  const isPrimative = !isArray && !isObject
+
+  if (isObject) {
+    const clonedObj = {}
+    Object.keys(val)
+          .forEach(key => {
+            clonedObj[key] = cloneDeep(val[key]);
+          })
+    return clonedObj;
+  }
+
+  if (isArray) {
+    return val.map(cloneDeep);
+  }
+
+  if (isPrimative) {
+    return val;
+  }
+}
+
 describe('clone object', function () {
   it('should clone an object', function () {
     var expected = {name: 'Ahmed', age: 27, skills: ['cycling', 'walking', 'eating']},
-        obj = {};
+        obj = cloneShallow(expected)
 
     expect(obj).toEqual(expected);
     expect(obj).not.toBe(expected);
--- test/flatten-array.js
+++ test/flatten-array.js
@@ -1,6 +1,17 @@
+
+const isArray = potentialArray => Array.isArray(potentialArray)
+
+function flatten(arrayToFlatten) {
+    let flattened = []
+    const pushToFlattened = val => isArray(val)
+          ? val.forEach(pushToFlattened)
+          : flattened.push(val);
+    pushToFlattened(arrayToFlatten)
+    return flattened.sort();
+}
 describe('flatten array', function () {
   it('should flatten an array', function () {
-    var arr = [1, 2, [1, 2, [3, 4, 5, [1]]], 2, [2]],
+    var arr = flatten([1, 2, [1, 2, [3, 4, 5, [1]]], 2, [2]]),
         expected = [1, 1, 1, 2, 2, 2, 2, 3, 4, 5];
 
     expect(arr).toEqual(expected);
